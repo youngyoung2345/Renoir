0f839d421bbbb9717f2e1aa4df8e844b536ddf19
youngyoung <lhayoung030425@gmail.com>
2024-06-22 20:27:16 +0900

    Add Model.py file

---
+++ Renoir 3.0/Model.py
@@ -0,0 +1,15 @@
+from Backbone import VGG19
+from ArcFace import ArcFace
+import torch.nn as nn
+
+class Recognizer(nn.Module):
+    def __init__(self):
+        super(Recognizer, self).__init__()
+        self.VGG19 = VGG19()
+        self.ArcFace = ArcFace(in_dim = 4096, out_dim = 20, s = 64, m = 0.5)
+    
+    def forward(self, x):
+        x = self.VGG19(x)
+        x = self.ArcFace(x)
+        
+        return x
