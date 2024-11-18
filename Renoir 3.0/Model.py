c4bf698400737612a2dd90e4cdff0ba4e6c186f2
young <134286859+youngyoung2345@users.noreply.github.com>
2024-11-18 14:33:38 +0900

    Update Model.py

--- Renoir 3.0/Model.py
+++ Renoir 3.0/Model.py
@@ -1,12 +1,12 @@
 from Backbone import VGG19
-from ArcFace import ArcFace
+from ArcFace import CosFace
 import torch.nn as nn
 
 class Recognizer(nn.Module):
     def __init__(self):
         super(Recognizer, self).__init__()
         self.VGG19 = VGG19()
-        self.ArcFace = ArcFace(in_dim = 25088, out_dim = 20, s = 64, m = 0.6)
+        self.CosFace = CosFace(in_dim = 25088, out_dim = 20, s = 64, m = 0.6)
     
     def forward(self, x):
         x = self.VGG19(x)
